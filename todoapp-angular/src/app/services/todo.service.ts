410518a58d47c1168c4cc7f0f00e8ca05592819f
warconn <tyler.conn@hotmail.com>
2019-01-22 21:55:45 -0500

    Working ToDo App

--- todoapp-angular/src/app/services/todo.service.ts
+++ todoapp-angular/src/app/services/todo.service.ts
@@ -1,9 +1,9 @@
 import ToDo from '../models/todo.model';
-import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs/Rx';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import {Response} from '@angular/http';
 import { Injectable } from '@angular/core';
 
-//RxJS operator for mapping the observable
 
 import 'rxjs/add/operator/map';
 
@@ -18,30 +18,23 @@ export class TodoService {
   ) { }
 
 
-  //Create todo, takes a ToDo Object
   createTodo(todo: ToDo): Observable<any>{
-    //returns the observable of http post request 
     return this.http.post(`${this.todoUrl}`, todo);
   }
 
-  //Read todo, takes no arguments
   getToDos(): Observable<ToDo[]>{
     return this.http.get(this.todoUrl)
     .map(res  => {
-      //Maps the response object sent from the server
-        
       return res["data"].docs as ToDo[];
     })
   }
-  //Update todo, takes a ToDo Object as parameter
+
   editTodo(todo:ToDo){
     let editUrl = `${this.todoUrl}`
-    //returns the observable of http put request 
     return this.http.put(editUrl, todo);
   }
 
   deleteTodo(id:string):any{
-    //Delete the object by the id
     let deleteUrl = `${this.todoUrl}/${id}`
     return this.http.delete(deleteUrl)
     .map(res  => {
@@ -49,7 +42,6 @@ export class TodoService {
     })
   }
 
-  //Default Error handling method.
   private handleError(error: any): Promise<any> {
     console.error('An error occurred', error); // for demo purposes only
     return Promise.reject(error.message || error);
