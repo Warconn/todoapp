0ae07c583df78da13d68ad1b544e657676bd30ba
warconn <tyler.conn@hotmail.com>
2019-01-22 21:13:35 -0500

    Prepared base project

--- todoapp-angular/src/app/app.module.ts
+++ todoapp-angular/src/app/app.module.ts
@@ -1,8 +1,11 @@
+import { FormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -10,9 +13,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    NgbModule.forRoot()
   ],
-  providers: [],
+  providers: [
+    
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
