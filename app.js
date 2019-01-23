e587e51ee195b89dcedf17fe5552217635af58c3
warconn <tyler.conn@hotmail.com>
2019-01-22 20:22:13 -0500

    Successful connection to local mongoDB

--- app.js
+++ app.js
@@ -9,6 +9,25 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+var bluebird = require('bluebird')
+
+//Start of mongo connection
+var mongoose = require('mongoose')
+mongoose.Promise = bluebird
+mongoose.connect('mongodb://127.0.0.1:27017/todoapp', { useNewUrlParser:true })
+.then(()=> { console.log(`Succesfully Connected to the
+Mongodb Database  at URL : mongodb://127.0.0.1:27017/todoapp`)})
+.catch(()=> { console.log(`Error Connecting to the Mongodb 
+Database at URL : mongodb://127.0.0.1:27017/todoapp`)})
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  next();
+});
+//End of the mongo connection
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
