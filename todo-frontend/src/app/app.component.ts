3919ff294dde9544e3c0bad18934a56a7f4a659c
warconn <tyler.conn@hotmail.com>
2019-04-08 22:20:41 -0400

    Add and delete client side

--- todo-frontend/src/app/app.component.ts
+++ todo-frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { trigger, state, transition, animate, style } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
@@ -14,5 +15,24 @@ export class AppComponent {
     console.log(value + " was added to array")
     console.log(this.todoArray)
   }
+
+  /*delete item*/
+  deleteItem(todo){
+    for(let i=0 ;i<= this.todoArray.length ;i++){
+     if(todo== this.todoArray[i]){
+      this.todoArray.splice(i,1)
+     }
+    }
+   }
+
+   // submit Form
+   todoSubmit(value:any){
+      if(value!==""){
+        this.todoArray.push(value.todo)
+        //this.todoForm.reset()
+      }else{
+        alert('Field required **')
+      }
+    }
 
 }
